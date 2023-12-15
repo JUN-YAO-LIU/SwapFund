d2324c370329daae46bd0bccb5a88fa7a828e0ca
JimLiu <jim.liu123@ehsn.com.tw>
2023-12-15 17:43:28 +0800

    add withdrawal

--- src/SwapFund.sol
+++ src/SwapFund.sol
@@ -18,6 +18,8 @@ contract SwapFund {
     // user => token => amount
     mapping(address => mapping(address => uint)) public ownerAssets;
 
+    mapping(address => address[]) public ownerAssetsTokenAddress;
+
     constructor(address factory,address router,address usdc){
         owner = msg.sender;
         _UNISWAP_FACTORY = factory;
@@ -35,9 +37,8 @@ contract SwapFund {
 
     function createFund(address[] memory tokens,uint[] memory amounts) public {
         address[] memory paths = new address[](2);
+        IERC20(USDC).approve(_UNISWAP_ROUTER,type(uint).max);
         for (uint i=0; i<tokens.length; i++) {
-
-            IERC20(USDC).approve(_UNISWAP_ROUTER,type(uint).max);
             
             paths[0] = USDC;
             paths[1] = tokens[i];
@@ -46,17 +47,56 @@ contract SwapFund {
             (uint reserve0,uint reserve1,) =  IUniswapV2Pair(pool).getReserves();
 
             uint tempAmount = IUniswapV2Router01(_UNISWAP_ROUTER).getAmountOut(amounts[i],reserve0,reserve1);
-            IUniswapV2Router01(_UNISWAP_ROUTER).swapExactTokensForTokens(
+            uint[] memory amountOut = IUniswapV2Router01(_UNISWAP_ROUTER).swapExactTokensForTokens(
                 amounts[i],
                 tempAmount, // min out
                 paths,
                 address(this), 
                 block.timestamp
             );
+
+            poolTokenAmount[paths[1]] += amountOut[1];
+            ownerAssets[msg.sender][paths[1]] += amountOut[1];
+            ownerAssetsTokenAddress[msg.sender].push(paths[1]);
         }
     }
 
-    function withdrawalFund() public {}
+    function withdrawalFund(address token) public {
+        address[] memory paths = new address[](2);
+
+        // get all value in the pool
+        for(uint i = 0; ownerAssetsTokenAddress[msg.sender].length > i;i++){
+            address tempToken = ownerAssetsTokenAddress[msg.sender][i];
+            uint256 amountIn = ownerAssets[msg.sender][tempToken];
+
+            paths[0] = tempToken;
+            paths[1] = token;
+
+            address pool = IUniswapV2Factory(_UNISWAP_FACTORY).getPair(paths[0], paths[1]);
+            (uint reserve0,uint reserve1,) =  IUniswapV2Pair(pool).getReserves();
+            uint tempAmount = IUniswapV2Router01(_UNISWAP_ROUTER).getAmountOut(amountIn,reserve0,reserve1);
+           // uint256 tempAmount = getPriceWithToken(tempToken,paths[1],amountIn);
+
+           
+           
+           if(token != tempToken){
+                IERC20(tempToken).approve(_UNISWAP_ROUTER,tempAmount);
+                uint[] memory amountOut = IUniswapV2Router01(_UNISWAP_ROUTER).swapExactTokensForTokens(
+                    amountIn,
+                    tempAmount, // min out
+                    paths,
+                    msg.sender, 
+                    block.timestamp
+                );
+           }
+        }
+    }
+
+    function getPriceWithToken(address token0,address token1,uint256 amountIn) public view returns(uint){
+       address pool = IUniswapV2Factory(_UNISWAP_FACTORY).getPair(token0,token1);
+       (uint256 _tokenIn, uint256 _tokenOut,) = IUniswapV2Pair(pool).getReserves();
+       return _getAmountOut(amountIn,_tokenIn,_tokenOut);
+    }
 
     function getPrice(address pool,uint256 amountIn) public view returns(uint){
        // address poolAddr = IUniswapV2Factory(factory).getPair(usdt,token);
--- test/SwapFund.t.sol
+++ test/SwapFund.t.sol
@@ -10,6 +10,7 @@ contract SwapFundTest is FlashSwapSetUp {
 
     address maker = makeAddr("maker");
     address user1 = makeAddr("user1");
+    address user2 = makeAddr("user2");
     SwapFund swapFund;
 
     function setUp() public override {
@@ -18,33 +19,33 @@ contract SwapFundTest is FlashSwapSetUp {
         // mint 100 ETH, 10000 USDC to maker
         vm.deal(maker, 100 ether);
         usdc.mint(maker, 10_000_000 * 10 ** usdc.decimals());
-        matic.mint(maker, 10_000 * 10 ** matic.decimals());
-        op.mint(maker, 10_000 * 10 ** op.decimals());
-        sol.mint(maker, 1000 * 10 ** op.decimals());
+        matic.mint(maker, 10_000_000 * 10 ** matic.decimals());
+        op.mint(maker, 10_000_000 * 10 ** op.decimals());
+        sol.mint(maker, 10_000_000 * 10 ** op.decimals());
 
         // maker provide liquidity to wethUsdcPool, wethUsdcSushiPool
         vm.startPrank(maker);
         // maker provide 50 ETH, 4000 USDC to wethUsdcPool
-        usdc.approve(address(uniswapV2Router), 4_000 * 10 ** usdc.decimals());
-        matic.approve(address(uniswapV2Router), 4_000 * 10 ** matic.decimals());
+        usdc.approve(address(uniswapV2Router), 8_000 * 10 ** usdc.decimals());
+        matic.approve(address(uniswapV2Router), 9_000 * 10 ** matic.decimals());
         uniswapV2Router.addLiquidity(
             address(usdc),
             address(matic),
-            4_000 * 10 ** usdc.decimals(),
-            4_000 * 10 ** usdc.decimals(),
+            8_000 * 10 ** usdc.decimals(),
+            9_000 * 10 ** usdc.decimals(),
             0,
             0,
             maker,
             block.timestamp
         );
 
-        usdc.approve(address(uniswapV2Router), 4_000 * 10 ** usdc.decimals());
-        op.approve(address(uniswapV2Router), 3_000 * 10 ** op.decimals());
+        usdc.approve(address(uniswapV2Router), 15_000 * 10 ** usdc.decimals());
+        op.approve(address(uniswapV2Router), 23_000 * 10 ** op.decimals());
         uniswapV2Router.addLiquidity(
             address(usdc),
             address(op),
-            4_000 * 10 ** usdc.decimals(),
-            3_000 * 10 ** usdc.decimals(),
+            15_000 * 10 ** usdc.decimals(),
+            23_000 * 10 ** usdc.decimals(),
             0,
             0,
             maker,
@@ -71,7 +72,12 @@ contract SwapFundTest is FlashSwapSetUp {
         usdc.mint(user1, 10_000_000 * 10 ** usdc.decimals());
         vm.stopPrank();
 
+        vm.startPrank(user2);
+        usdc.mint(user2, 10_000_000 * 10 ** usdc.decimals());
+        vm.stopPrank();
+
         vm.label(user1, "User1");
+        vm.label(user2, "User2");
         vm.label(address(swapFund), "swapFund");
     }
 
@@ -79,37 +85,91 @@ contract SwapFundTest is FlashSwapSetUp {
         vm.startPrank(user1);
         usdc.approve(address(swapFund), 4_000 * 10 ** usdc.decimals());
         swapFund.deposit(address(usdc));
+        vm.stopPrank();
 
         console2.log("swapFund usdc:",usdc.balanceOf(address(swapFund)));
         console2.log("create matic markets:",matic.balanceOf(address(maticUsdcPool)));
 
+        assertEq(usdc.balanceOf(address(swapFund)),4_000 * 10 ** usdc.decimals());
+    }
+
+    function test_swapToMultipleTokens() public {
+        test_depositIntoMarkets();
+
+        vm.startPrank(user1);
         address[] memory tokens = new address[](2);
         uint[] memory amounts = new uint[](2);
 
         // amount in
-        amounts[0] = 123;
-        amounts[1] = 56;
+        amounts[0] = 4000;
+        amounts[1] = 1500;
 
         tokens[0] = address(matic);
         tokens[1] = address(sol);
+        
+        uint beforeMatic = matic.balanceOf(address(swapFund));
+        uint beforeSol = sol.balanceOf(address(swapFund));
 
         swapFund.createFund(tokens,amounts);
         console2.log("create matic markets:",matic.balanceOf(address(swapFund)));
         console2.log("create sol markets:",sol.balanceOf(address(swapFund)));
 
+        uint afterMatic = matic.balanceOf(address(swapFund));
+        uint afterSol = sol.balanceOf(address(swapFund));
+
+        assertTrue(afterMatic > 0 , "matic less than zero");
+        assertTrue(afterSol > 0 ,"sol less than zero");
+
+        console2.log("user matic:",swapFund.ownerAssets(user1,address(matic)));
+        console2.log("user sol:",swapFund.ownerAssets(user1,address(sol)));
+
+        // console2.log("user1 fund token address:",swapFund.ownerAssetsTokenAddress(user1,1));
+
+        assertEq(swapFund.ownerAssets(user1,address(matic)), afterMatic - beforeMatic ,
+        "tow value should be same.");
+        assertEq(swapFund.ownerAssets(user1,address(sol)), afterSol - beforeSol ,
+        "tow value should be same.");
         vm.stopPrank();
-
-        assertTrue(matic.balanceOf(address(swapFund)) > 0 , "matic less than zero");
-        assertTrue(sol.balanceOf(address(swapFund)) > 0 ,"sol less than zero");
-    }
-
-    function test_swapToMultipleTokens() public {
-    }
-
-    function test_getPricesFromUni() public {
     }
 
     function test_withdrawalTokens() public {
+
+        test_swapToMultipleTokens();
+
+        vm.startPrank(user2);
+        usdc.approve(address(swapFund), 10_000 * 10 ** usdc.decimals());
+        swapFund.deposit(address(usdc));
+
+        address[] memory tokens = new address[](3);
+        uint[] memory amounts = new uint[](3);
+
+        // amount in
+        amounts[0] = 1000;
+        amounts[1] = 2000;
+        amounts[2] = 3000;
+
+        tokens[0] = address(matic);
+        tokens[1] = address(sol);
+        tokens[2] = address(op);
+        
+        swapFund.createFund(tokens,amounts);
+
+        // console2.log("user2 matic:",swapFund.ownerAssets(user2,address(matic)));
+        // console2.log("user2 sol:",swapFund.ownerAssets(user2,address(sol)));
+        // console2.log("user2 op:",swapFund.ownerAssets(user2,address(op)));
+
+        vm.stopPrank();
+
+        vm.startPrank(user1);
+        console2.log("user2 op:",swapFund.ownerAssets(user2,address(op)));
+        console2.log("user1 token address 0 :",swapFund.ownerAssetsTokenAddress(user1,0));
+        console2.log("user1 token address 1 :",swapFund.ownerAssetsTokenAddress(user1,1));
+
+        swapFund.withdrawalFund(address(op));
+        vm.stopPrank();
+    }
+
+    function test_getPricesFromUni() public {
     }
 
     function test_getMarketSupportTokens() public {
