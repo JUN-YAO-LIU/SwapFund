073c0e6bedd65694d61e21697ef275108aee05e2
劉駿嶢 <liujunyao@liujunyaodeMacBook-Pro.local>
2024-01-01 23:40:23 +0800

    add web2

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes, Link } from "react-router-dom";
 import { ethers } from "ethers";
+import Borrow from './Borrow';
 
 function App() {
   let signer = null;
@@ -11,24 +13,20 @@ function App() {
     console.log(provider);
   }
 
-
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>Hello</h1>
+        <ul className='App-link'>
+          <li>
+            <Link to='/'>home</Link>
+          </li>
+          <li>
+            <Link to='/borrow'>borrow</Link>
+          </li>
+        </ul>
+        <Routes>
+          <Route path='borrow' element={<Borrow />} />
+        </Routes>
     </div>
   );
 }
