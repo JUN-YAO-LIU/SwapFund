c8a2f5ef3c7ccdb75a3b14758d4c3eeebdeac18b
JimLiu <jim.liu123@ehsn.com.tw>
2024-01-04 14:32:52 +0800

    add ethers.js provider and signer

--- web2/init.js
+++ web2/init.js
@@ -1,7 +1,21 @@
-// const provider = new ethers.providers.Web3Provider(window.ethereum)
-// let testNumber = await provider.getBlockNumber()
+import data from "../out/SwapFund.sol/SwapFund.json" assert { type: 'json' }
 
 // console.log(testNumber)
 if (typeof window.ethereum !== 'undefined') {
     console.log('MetaMask is installed!');
 }
+
+
+const provider = new ethers.providers.Web3Provider(window.ethereum)
+const signer = provider.getSigner("0x570D01A5Bd431BdC206038f3cff8E17B22AA3662")
+
+const swapFund = new ethers.Contract("0x38A7410130C3aE2CC783A6B8461a1101955967FC", data.abi, signer)
+
+const name = await swapFund.name()
+const symbol = await swapFund.symbol()
+const decimals = await swapFund.decimals()
+const totalSupply = await swapFund.totalSupply()
+
+console.log(swapFund)
+console.log(symbol)
+console.log(totalSupply)
