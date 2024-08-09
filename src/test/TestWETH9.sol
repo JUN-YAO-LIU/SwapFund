70bd5899e8c3704195ca963cad197d1eb0fb07df
JimLiu <jim.liu123@ehsn.com.tw>
2024-08-09 17:22:55 +0800

    modify remappings

--- src/test/TestWETH9.sol
+++ src/test/TestWETH9.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { ERC20 } from "@openzeppelin/token/ERC20/ERC20.sol";
 import { IWETH9 } from "./interfaces/IWETH9.sol";
 
 contract TestWETH9 is IWETH9, ERC20("WETH", "WETH") {
